61299629336d091eb85b099b7b834e8e60f97773
Sérgio Campos <srabelocampos@gmail.com>
2020-08-15 21:20:11 -0300

    Adding external fonts (RobotoSLab). Creation of the initial application screen.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => (
-  <Container />
+  <Container>
+    <Image source={logoImg} />
+    <Title>Faça seu logon</Title>
+  </Container>
 );
 export default SignIn;
