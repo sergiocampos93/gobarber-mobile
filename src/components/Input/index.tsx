2ee022e8e17071ffbdd8c2f75423337ebd474384
Sérgio Campos <srabelocampos@gmail.com>
2020-08-20 00:18:22 -0300

    Input style changes. Now, the inputs color change when them are filled or selected.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,10 @@
 import React, {
-  useEffect, useRef, useImperativeHandle, forwardRef,
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
 } from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
@@ -29,6 +34,18 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
+
   useImperativeHandle(ref, () => ({
     focus() {
       inputElementRef.current.focus();
@@ -51,13 +68,15 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
     });
   }, [fieldName, registerField]);
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
       <TextInput
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={(value) => { inputValueRef.current.value = value; }}
         {...rest}
       />
